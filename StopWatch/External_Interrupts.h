c4fd497f114543b15684d2cbff92e3992a69508c
Marwan Fawzy Shahat Mahmoud <105066899+ArabianHindi@users.noreply.github.com>
2023-06-27 18:02:35 +0300

    Add files via upload

---
+++ StopWatch/External_Interrupts.h
@@ -0,0 +1,34 @@
+/******************************************************************************
+ * Module: External Interrupts
+ * File Name: External_Interrupts.h
+ * Description: Header file for The Eta32mini External Interrupts Driver.
+ * Author: Marwan Shehata
+ * Group:  76
+ *******************************************************************************/
+
+#ifndef EXTERNAL_INTERRUPTS_H_
+#define EXTERNAL_INTERRUPTS_H_
+
+#include "gpio.h"
+#include "avr/interrupt.h"
+
+/*******************************************************************************
+ *                                Definitions                                  *
+ *******************************************************************************/
+#define I_BIT   7
+
+/*******************************************************************************
+ *                              Functions Prototypes                           *
+ *******************************************************************************/
+
+/* External INT0 enable and configuration function */
+void INT0_Init(void);
+
+/* External INT1 enable and configuration function */
+void INT1_Init(void);
+
+/* External INT2 enable and configuration function */
+void INT2_Init(void);
+
+
+#endif /* EXTERNAL_INTERRUPTS_H_ */
